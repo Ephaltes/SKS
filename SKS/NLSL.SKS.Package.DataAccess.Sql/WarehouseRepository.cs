c765131f651ed421b93d82d9492752486a48fc57
rothexD <56934789+rothexD@users.noreply.github.com>
2021-11-06 23:24:54 +0100

    added logging

--- SKS/NLSL.SKS.Package.DataAccess.Sql/WarehouseRepository.cs
+++ SKS/NLSL.SKS.Package.DataAccess.Sql/WarehouseRepository.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 
+using Microsoft.Extensions.Logging;
+
 using NLSL.SKS.Package.DataAccess.Entities;
 using NLSL.SKS.Package.DataAccess.Interfaces;
 
@@ -10,37 +12,50 @@ namespace NLSL.SKS.Package.DataAccess.Sql
     public class WarehouseRepository : IWarehouseRepository
     {
         private readonly PackageContext _context;
-        public WarehouseRepository(PackageContext context)
+        private readonly ILogger<WarehouseRepository> _logger;
+        public WarehouseRepository(PackageContext context, ILogger<WarehouseRepository> logger)
         {
             _context = context;
+            _logger = logger;
         }
         public string Create(Warehouse warehouse)
         {
+            _logger.LogDebug("starting, create warehouse");
             _context.Warehouses.Add(warehouse);
             _context.SaveChanges();
-
+            _logger.LogDebug("create warehouse complete");
             return warehouse.Code;
         }
         public void Update(Warehouse warehouse)
         {
+            _logger.LogDebug("starting, update warehouse");
             _context.Warehouses.Update(warehouse);
             _context.SaveChanges();
+            _logger.LogDebug("update warehouse complete");
         }
         public void Delete(string id)
         {
+            _logger.LogDebug("starting, delete warehouse");
             Warehouse temp = new Warehouse
                               { Code = id };
 
             _context.Warehouses.Remove(temp);
             _context.SaveChanges();
+            _logger.LogDebug("delete warehouse complete");
         }
         public IReadOnlyCollection<Warehouse> GetAllWarehouses()
         {
-            return _context.Warehouses.ToList();
+            _logger.LogDebug("starting, get all warehouses");
+            var warehouses = _context.Warehouses.ToList();
+            _logger.LogDebug("get all warehouses complete");
+            return warehouses;
         }
         public Warehouse? GetWarehouseByCode(string code)
         {
-            return _context.Warehouses.FirstOrDefault(warehouse => warehouse.Code == code);
+            _logger.LogDebug("starting, get warehouse by code");
+            var warehouse = _context.Warehouses.FirstOrDefault(warehouse => warehouse.Code == code);
+            _logger.LogDebug("get warehouse by code complete");
+            return warehouse;
         }
     }
 }
