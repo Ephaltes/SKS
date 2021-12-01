c31ee2dfc0b0750f126cf54eebd2e07cb607812e
rothexD <56934789+rothexD@users.noreply.github.com>
2021-12-01 18:59:21 +0100

    unit test fix

--- SKS/NLSL.SKS.Package.BusinessLogic.Tests/WebHookValidatorBehaviour.cs
+++ SKS/NLSL.SKS.Package.BusinessLogic.Tests/WebHookValidatorBehaviour.cs
@@ -1,4 +1,7 @@
-﻿using FluentValidation.TestHelper;
+﻿using System;
+using System.Security.Policy;
+
+using FluentValidation.TestHelper;
 
 using NLSL.SKS.Package.BusinessLogic.Entities;
 using NLSL.SKS.Package.BusinessLogic.Validators;
@@ -21,23 +24,29 @@ namespace NLSL.SKS.Package.BusinessLogic.Tests
         {
             WebHook model = new WebHook();
             TestValidationResult<WebHook> result = validator.TestValidate(model);
-            result.ShouldHaveValidationErrorFor(entity => entity.Id);
+            result.ShouldHaveValidationErrorFor(entity => entity.URL);
         }
         [Test]
         public void WebHookValidator_TrackingIdIsValid_Success()
         {
             WebHook model = new WebHook();
             model.trackingId = "ABCDEFGHI";
+            model.URL = "test";
+            model.Id = 1L;
+            model.CreatedAt = DateTime.Now;
             TestValidationResult<WebHook> result = validator.TestValidate(model);
-            result.ShouldNotHaveValidationErrorFor(entity => entity.Id);
+            result.ShouldNotHaveValidationErrorFor(entity => entity.trackingId);
         }
         [Test]
         public void WebHookValidator_TrackingIdIsNotValid_ValidationError()
         {
             WebHook model = new WebHook();
             model.trackingId = "ABCDEFGH";
+            model.URL = "test";
+            model.Id = 1L;
+            model.CreatedAt = DateTime.Now;
             TestValidationResult<WebHook> result = validator.TestValidate(model);
-            result.ShouldHaveValidationErrorFor(entity => entity.Id);
+            result.ShouldHaveValidationErrorFor(entity => entity.trackingId);
         }
     }
 }
