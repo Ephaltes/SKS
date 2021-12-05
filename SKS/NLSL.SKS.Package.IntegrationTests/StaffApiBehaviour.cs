cbc7a92487a873fcccacad049f6d57eb0e065f67
Ephaltes <ngoc.lam@live.at>
2021-12-05 21:08:28 +0100

    added newtonsoft to integrationtest

--- SKS/NLSL.SKS.Package.IntegrationTests/StaffApiBehaviour.cs
+++ SKS/NLSL.SKS.Package.IntegrationTests/StaffApiBehaviour.cs
@@ -17,7 +17,6 @@ using NLSL.SKS.Package.Services.DTOs;
 
 using NUnit.Framework;
 
-using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace NLSL.SKS.Package.IntegrationTests
 {
@@ -91,7 +90,7 @@ namespace NLSL.SKS.Package.IntegrationTests
             {
                 Assert.Fail();
             }
-            var parsedResponse = JsonSerializer.Deserialize<WebhookMessage>(await resultaAddWebhook.Content.ReadAsStringAsync());
+            var parsedResponse = JsonConvert.DeserializeObject<WebhookMessage>(await resultaAddWebhook.Content.ReadAsStringAsync());
             
             var webhooks = await _httpClient.GetAsync("/parcel/" + trackingID + "/webhooks");
 
