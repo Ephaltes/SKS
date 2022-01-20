fae8fabb53a16c9d1d422dc0fa79949d2797fd16
Ephaltes <ngoc.lam@live.at>
2022-01-20 18:46:50 +0100

    appsettings

--- SKS/NLSL.SKS.Package.BusinessLogic/Validators/RecipientValidator.cs
+++ SKS/NLSL.SKS.Package.BusinessLogic/Validators/RecipientValidator.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using FluentValidation;
+﻿using FluentValidation;
 
 using NLSL.SKS.Package.BusinessLogic.Entities;
 
@@ -14,28 +8,29 @@ namespace NLSL.SKS.Package.BusinessLogic.Validators
     {
         public RecipientValidator()
         {
-            RuleFor(p =>p.Country).NotNull().WithMessage("{PropertyName} was null");
+            RuleFor(p => p.Country).NotNull().WithMessage("{PropertyName} was null");
 
-            When(p => (p.Country.ToLower().Trim() == "austria") ||
-                      (p.Country.ToLower().Trim() == "österreich") || 
-                      (p.Country.ToLower().Trim() == "at") , () => {
-                
-                                                                         RuleFor(p => p.PostalCode)
-                                                                             .NotNull().WithMessage("{PropertyName} is Empty")
-                                                                             .Matches("^A-[0-9]{4}$").WithMessage("{PropertyName} does not Match ^A-[0-9]{4}$ Regex");
-                
-                                                                         RuleFor(p => p.Street)
-                                                                             .NotNull().WithMessage("{PropertyName} is Empty")
-                                                                             .Matches("^[A-Za-zßöÖäÄüÜ]+ [A-Za-z0-9öÖäÄüÜ/-]+$").WithMessage("{PropertyName} does not Match ^[A-Za-zßöÖäÄüÜ]+ [A-Za-z0-9öÖäÄüÜ/]+$ Regex");
-                
-                                                                         RuleFor(p => p.City)
-                                                                             .NotNull().WithMessage("{PropertyName} is Empty")
-                                                                             .Matches("^[A-ZÖÄÜ]{1}[A-Za-zöÖäÄüÜ -]*$").WithMessage("{PropertyName} does not Match ^[A-ZÖÄÜ]{1}[A-Za-zöÖäÄüÜ -]*$ Regex");
-                
-                                                                         RuleFor(p => p.Name)
-                                                                             .NotNull().WithMessage("{PropertyName} is Empty")
-                                                                             .Matches("^[A-ZÖÄÜ]{1}[A-Za-zöÖäÄüÜ -]*$").WithMessage("{PropertyName} does not Match ^[A-ZÖÄÜ]{1}[A-Za-zöÖäÄüÜ -]*$ Regex");
-                                                                     });
+            When(p => p.Country.ToLower().Trim() == "austria" ||
+                      p.Country.ToLower().Trim() == "österreich" ||
+                      p.Country.ToLower().Trim() == "at",
+                () =>
+                {
+                    RuleFor(p => p.PostalCode)
+                        .NotNull().WithMessage("{PropertyName} is Empty")
+                        .Matches("^(A-)?[0-9]{4}$").WithMessage("{PropertyName} does not Match ^(A-)?[0-9]{4}$ Regex");
+
+                    RuleFor(p => p.Street)
+                        .NotNull().WithMessage("{PropertyName} is Empty")
+                        .Matches("^[A-Za-z0-9öÖäÄüÜ/ -]+$").WithMessage("{PropertyName} does not Match ^[A-Za-z0-9öÖäÄüÜ /]+$ Regex");
+
+                    RuleFor(p => p.City)
+                        .NotNull().WithMessage("{PropertyName} is Empty")
+                        .Matches("^[A-Za-zöÖäÄüÜ -]+$").WithMessage("{PropertyName} does not Match ^[A-Za-zöÖäÄüÜ -]+$ Regex");
+
+                    RuleFor(p => p.Name)
+                        .NotNull().WithMessage("{PropertyName} is Empty")
+                        .Matches("^[A-Za-zöÖäÄüÜ -]+$").WithMessage("{PropertyName} does not Match ^[A-Za-zöÖäÄüÜ -]+$ Regex");
+                });
         }
-    } 
+    }
 }
